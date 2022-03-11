777e20e3cd8dc1ad0d6d0548fbc8f5c9b5f9d73e
Valter F.C <6312421+valterfc@users.noreply.github.com>
2022-03-11 15:48:39 -0300

    User Insert Validation

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,12 +2,20 @@
 
 @section('title', 'Novo Usuário')
 
+@if ($errors->any())
+    <ul class="errors">
+        @foreach ($errors->all() as $error)
+            <li class="error">{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 @section('content')
     <h1>Novo Usuário</h1>
     <form action="{{ route('users.store') }}" method="post">
         @csrf
-        <input type="text" name="name" placeholder="Nome:">
-        <input type="email" name="email" placeholder="Email:">
+        <input type="text" name="name" placeholder="Nome:" value="{{ old('name') }}">
+        <input type="email" name="email" placeholder="Email:" value="{{ old('email') }}">
         <input type="password" name="password" placeholder="Senha:">
         <button type="submit">Enviar</button>
     </form>

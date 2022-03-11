faeece4d0cd4374988fb93634211132e37a8270f
Valter F.C <6312421+valterfc@users.noreply.github.com>
2022-03-11 18:18:34 -0300

    Add Tailwind CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Curso Laravel 9 - @yield('title')</title>
+    <link rel="shortcut icon" href="{{ url('images/favicon.ico') }}" type="image/png">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-gray-50">
 
-    <div class="app">
+    <div class="container mx-auto px-4 py-8">
         @yield('content')
     </div>
 
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
-@section('title', "Listagem do Usuário { $user->name }")
+@section('title', 'Listagem do Usuário')
 
 @section('content')
-    <h1>Listagem do Usuário {{ $user->name }}</h1>
+<h1 class="text-2xl font-semibold leading-tigh py-2">Listagem do usuário {{ $user->name }}</h1>
 
-    <ul>
-        <li>{{ $user->name }}</li>
-        <li>{{ $user->email }}</li>
-        <li>{{ $user->created_at }}</li>
-    </ul>
+<ul>
+    <li>{{ $user->name }}</li>
+    <li>{{ $user->email }}</li>
+</ul>
 
-    <form action="{{ route('users.destroy', $user->id) }}" method="post">
-        @method('DELETE')
-        @csrf
-        <button type="submit">Deletar</button>
-    </form>
+<form action="{{ route('users.destroy', $user->id) }}" method="POST" class="py-12">
+    @method('DELETE')
+    @csrf
+    <button type="submit" class="rounded-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">Deletar</button>
+</form>
+
 @endsection
